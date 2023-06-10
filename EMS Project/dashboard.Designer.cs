2595f964e5cb21ef6df0a72540affaea3e2bae4d
Usama Javed <usamajaved1928@gmail.com>
2023-06-10 03:23:49 -0700

    Add project files.

---
+++ EMS Project/dashboard.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace EMS_Project
+{
+    partial class mainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnDashKbaIRAC = new System.Windows.Forms.Button();
+            this.btnDashKbaMachine = new System.Windows.Forms.Button();
+            this.btnDashKomoriL528 = new System.Windows.Forms.Button();
+            this.btnDashSM102 = new System.Windows.Forms.Button();
+            this.btnDashPaperBag = new System.Windows.Forms.Button();
+            this.btnDashKomoriL528AC = new System.Windows.Forms.Button();
+            this.btnDashSM1028AC = new System.Windows.Forms.Button();
+            this.btnDashSM1028 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Arial", 16F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 44);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "EMS Dashboard";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BtnDashKbaIRAC
+            // 
+            this.BtnDashKbaIRAC.Font = new System.Drawing.Font("Arial", 12F);
+            this.BtnDashKbaIRAC.Location = new System.Drawing.Point(12, 47);
+            this.BtnDashKbaIRAC.Name = "BtnDashKbaIRAC";
+            this.BtnDashKbaIRAC.Size = new System.Drawing.Size(143, 34);
+            this.BtnDashKbaIRAC.TabIndex = 2;
+            this.BtnDashKbaIRAC.Text = "KBA IR-AC";
+            this.BtnDashKbaIRAC.UseVisualStyleBackColor = true;
+            // 
+            // btnDashKbaMachine
+            // 
+            this.btnDashKbaMachine.Font = new System.Drawing.Font("Arial", 12F);
+            this.btnDashKbaMachine.Location = new System.Drawing.Point(12, 87);
+            this.btnDashKbaMachine.Name = "btnDashKbaMachine";
+            this.btnDashKbaMachine.Size = new System.Drawing.Size(143, 34);
+            this.btnDashKbaMachine.TabIndex = 3;
+            this.btnDashKbaMachine.Text = "KBA Machine";
+            this.btnDashKbaMachine.UseVisualStyleBackColor = true;
+            // 
+            // btnDashKomoriL528
+            // 
+            this.btnDashKomoriL528.Font = new System.Drawing.Font("Arial", 12F);
+            this.btnDashKomoriL528.Location = new System.Drawing.Point(12, 127);
+            this.btnDashKomoriL528.Name = "btnDashKomoriL528";
+            this.btnDashKomoriL528.Size = new System.Drawing.Size(143, 34);
+            this.btnDashKomoriL528.TabIndex = 4;
+            this.btnDashKomoriL528.Text = "Komori L528";
+            this.btnDashKomoriL528.UseVisualStyleBackColor = true;
+            // 
+            // btnDashSM102
+            // 
+            this.btnDashSM102.Font = new System.Drawing.Font("Arial", 12F);
+            this.btnDashSM102.Location = new System.Drawing.Point(12, 247);
+            this.btnDashSM102.Name = "btnDashSM102";
+            this.btnDashSM102.Size = new System.Drawing.Size(143, 34);
+            this.btnDashSM102.TabIndex = 7;
+            this.btnDashSM102.Text = "SM 102";
+            this.btnDashSM102.UseVisualStyleBackColor = true;
+            // 
+            // btnDashPaperBag
+            // 
+            this.btnDashPaperBag.Font = new System.Drawing.Font("Arial", 12F);
+            this.btnDashPaperBag.Location = new System.Drawing.Point(12, 207);
+            this.btnDashPaperBag.Name = "btnDashPaperBag";
+            this.btnDashPaperBag.Size = new System.Drawing.Size(143, 34);
+            this.btnDashPaperBag.TabIndex = 6;
+            this.btnDashPaperBag.Text = "Paper Bag";
+            this.btnDashPaperBag.UseVisualStyleBackColor = true;
+            // 
+            // btnDashKomoriL528AC
+            // 
+            this.btnDashKomoriL528AC.Font = new System.Drawing.Font("Arial", 12F);
+            this.btnDashKomoriL528AC.Location = new System.Drawing.Point(12, 167);
+            this.btnDashKomoriL528AC.Name = "btnDashKomoriL528AC";
+            this.btnDashKomoriL528AC.Size = new System.Drawing.Size(143, 34);
+            this.btnDashKomoriL528AC.TabIndex = 5;
+            this.btnDashKomoriL528AC.Text = "Komori L528 AC";
+            this.btnDashKomoriL528AC.UseVisualStyleBackColor = true;
+            // 
+            // btnDashSM1028AC
+            // 
+            this.btnDashSM1028AC.Font = new System.Drawing.Font("Arial", 12F);
+            this.btnDashSM1028AC.Location = new System.Drawing.Point(12, 327);
+            this.btnDashSM1028AC.Name = "btnDashSM1028AC";
+            this.btnDashSM1028AC.Size = new System.Drawing.Size(143, 34);
+            this.btnDashSM1028AC.TabIndex = 9;
+            this.btnDashSM1028AC.Text = "SM 102.8 AC";
+            this.btnDashSM1028AC.UseVisualStyleBackColor = true;
+            // 
+            // btnDashSM1028
+            // 
+            this.btnDashSM1028.Font = new System.Drawing.Font("Arial", 12F);
+            this.btnDashSM1028.Location = new System.Drawing.Point(12, 287);
+            this.btnDashSM1028.Name = "btnDashSM1028";
+            this.btnDashSM1028.Size = new System.Drawing.Size(143, 34);
+            this.btnDashSM1028.TabIndex = 8;
+            this.btnDashSM1028.Text = "SM 102.8";
+            this.btnDashSM1028.UseVisualStyleBackColor = true;
+            // 
+            // mainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDashSM1028AC);
+            this.Controls.Add(this.btnDashSM1028);
+            this.Controls.Add(this.btnDashSM102);
+            this.Controls.Add(this.btnDashPaperBag);
+            this.Controls.Add(this.btnDashKomoriL528AC);
+            this.Controls.Add(this.btnDashKomoriL528);
+            this.Controls.Add(this.btnDashKbaMachine);
+            this.Controls.Add(this.BtnDashKbaIRAC);
+            this.Controls.Add(this.label1);
+            this.Name = "mainScreen";
+            this.Text = "Dashboard";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnDashKbaIRAC;
+        private System.Windows.Forms.Button btnDashKbaMachine;
+        private System.Windows.Forms.Button btnDashKomoriL528;
+        private System.Windows.Forms.Button btnDashSM102;
+        private System.Windows.Forms.Button btnDashPaperBag;
+        private System.Windows.Forms.Button btnDashKomoriL528AC;
+        private System.Windows.Forms.Button btnDashSM1028AC;
+        private System.Windows.Forms.Button btnDashSM1028;
+    }
+}
+
